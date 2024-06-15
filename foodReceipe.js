0d9a0941b06973dd0ad27e6cb7704a37e56ff60a
SDeepaGithub <Sdeepalakshmi23@gmail.com>
2024-06-15 20:12:38 +0530

    Food Recipe C2

--- foodReceipe.js
+++ foodReceipe.js
@@ -31,3 +31,16 @@ logoutButton.addEventListener('click', () => {
             console.error('Error Signing out:', error);
         })
 })
+
+var sidenavbar = document.querySelector(".side-navbar")
+
+
+function showNavbar() {
+    sidenavbar.style.left = "0"
+    sidenavbar.style.display = "block";
+}
+
+function closeNavbar() {
+    sidenavbar.style.left = "-60%"
+    sidenavbar.style.display = "none";
+}
